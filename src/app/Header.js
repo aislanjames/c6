dd869d9f2c510d6da1bd473a26560221b125cc9f
aislanjames <aislanjames@gmail.com>
2023-10-24 19:47:47 -0300

    Alteração na área do logo, no Header

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,13 +1,13 @@
 // src/app/Header.js
 import React from 'react';
-import { AppBar, Toolbar, Typography, Avatar } from '@mui/material';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 import Link from 'next/link';
 
 const Header = () => {
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <Link href="#"><Avatar alt="Logotipo C6 Bank" src="#" ></Avatar></Link>
+        <Typography variant='h6'><Link href="#" >C6 Bank</Link></Typography>
         <Link href="#">Início</Link>
         <Link href="#">Produtos</Link>
         <Link href="#">Seu Bolso</Link>
